c877d49f372e08835fa7f38f90e32ea979b78e11
Aakash B. Reddy <54330393+AakashReddy22@users.noreply.github.com>
2020-08-13 16:01:12 -0400

    Update DAO.java

--- hellofx/src/main/java/DAO.java
+++ hellofx/src/main/java/DAO.java
@@ -2,11 +2,9 @@
 public interface DAO {
    public static final String DB_URL =
 	  // Offering the program the database connection to find data for JavaFx	   
-      "jdbc:mysql://localhost:3306/soccerdb";
+      ("Absent for security reasons");
    public static final String DRIVER =
 	  // Offering the JDBC Driver
       "com.mysql.jdbc.Driver";
-   // Giving the program access to extract variables with User password, accessible in production mode
-   public static final String USER = "AppUser";
-   public static final String PASS = "Welcome@123";
+   
 }
